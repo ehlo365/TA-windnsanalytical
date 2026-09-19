d03a3e826096bff5fb7fa9caa518947f5579c2d7
Anders <93841724+ehlo365@users.noreply.github.com>
2026-09-19 14:32:02 +0200

    Enhance parameter descriptions and help messages in init_dns_analytics.ps1 for better usability

--- bin/init_dns_analytics.ps1
+++ bin/init_dns_analytics.ps1
@@ -1,12 +1,26 @@
-﻿
-param
-(
-      [Parameter(Mandatory = $false)] [string] $computername = "."
-    , [Parameter(Mandatory = $false)] [switch] $bounce
+﻿<#
+.PARAMETER ComputerName
+    Specify the target computer for which to initialize the DNS analytics.
+
+.PARAMETER Bounce
+    If specified, the Splunk Forwarder service will be restarted to apply changes.
+
+.PARAMETER MatchAnyKeyword
+    Specify which events to log for the ETW Trace Provider.
+    0x0000000000000003: QUERY_RECEIVED and RESPONSE_SUCCESS
+    0x0000000000000023: QUERY_RECEIVED, RESPONSE_SUCCESS and RECURSE_RESPONSE_IN
+
+#>
+param (
+    [Parameter(Mandatory = $false, HelpMessage="Specify the target computer for which to initialize the DNS analytics.")] 
+    [string]$ComputerName = $env:computername,
+    [Parameter(Mandatory = $false, HelpMessage="Restart the Splunk Forwarder service to apply changes.")]
+    [switch]$Bounce,
+    [Parameter(Mandatory = $false, HelpMessage="Keyword to match any DNS event.")]
+    [string]$MatchAnyKeyword = "0x0000000000000003"
 )
 
-
-$eventlogSettings = Get-WinEvent -ListLog 'Microsoft-Windows-DNSServer/Analytical'  -ComputerName $computername
+$eventlogSettings = Get-WinEvent -ListLog 'Microsoft-Windows-DNSServer/Analytical' -ComputerName $ComputerName
 
 # Disable and re-enable the log to clear it
 $eventlogSettings.IsEnabled = $false
@@ -20,17 +34,16 @@ $eventlogSettings.SaveChanges()
 
 $eventlogSettings
 
-# Modify ETW Trace Provider to only log QUERY_RECEIVED, RECURSE_RESPONSE_IN and RESPONSE_SUCCESS Events.
 try {
-	Set-EtwTraceProvider -Guid '{EB79061A-A566-4698-9119-3ED2807060E7}' -SessionName 'EventLog-Microsoft-Windows-DNSServer-Analytical' -MatchAnyKeyword "0x0000000000000023" -ErrorAction Stop
+	Set-EtwTraceProvider -Guid '{EB79061A-A566-4698-9119-3ED2807060E7}' -SessionName 'EventLog-Microsoft-Windows-DNSServer-Analytical' -MatchAnyKeyword $MatchAnyKeyword -ErrorAction Stop
 }
 catch {
 	#[Console]::Error.WriteLine(("INFO [{0}:{1}] Failed to modify ETW Trace Provider." -f $scriptname, $PID)) 
 }
 
-if($bounce)
+if($Bounce)
 {
-    Invoke-Command -Computer $computername -ScriptBlock {
+    Invoke-Command -Computer $ComputerName -ScriptBlock {
         Get-Service -Name "SplunkForwarder" -ErrorAction SilentlyContinue | Stop-Service
 		
 		# Clean up any stranded scripted input processes
@@ -43,4 +56,3 @@ if($bounce)
         Get-WmiObject  -Class Win32_Process -Filter "name = 'powershell.exe' AND CommandLine LIKE '%\\etc\\apps\\%\\bin\\get_dns_analytics.ps1%'"  | Select-Object ProcessID,CommandLine
     }
 }
-
